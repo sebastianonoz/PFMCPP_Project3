42bdf26fe94f0ffd935ad15163085e80768cdbb6
sebastianonoz <89497840+sebastianonoz@users.noreply.github.com>
2023-01-15 21:23:47 +0000

    1eStep10

--- main.cpp
+++ main.cpp
@@ -1575,6 +1575,82 @@ struct CarWash
     //detail the car interior
     void detailInterior();
 };
+
+struct Guitar
+{
+
+    // 5 properties:
+    //     1) Number of strings (int)
+    int numStrings = 6;
+    //     2) Number of frets (int)
+    int numFrets = 22;
+    //     3) Volume level (float)
+    float volumeLevel = 3.7f;
+    //     4) Brand (std::string)
+    std::string brand = "Gibson";
+    //     5) Material (std::string)
+    std::string material = "Spruce Wood";
+
+    struct Strings
+    {
+        // Low E string gauge
+        float lowEStringGauge = .040f; 
+
+        float dStringGauge = .010f;
+        std::string stringBrand = "Ernie Ball";
+        std::string sku = "2229";
+        std::string stringMaterial ="Nickel";
+        
+        void stringBend(std::string whichString);
+        void stringSlide(std::string targetNote);
+        void stringSnap(std::string whichString);
+    
+    };
+    // 3 things it can do:
+    //     1) Play individual notes
+    void playNote(std::string whichNote);
+    //     2) Be tuned to different tunings
+    void tuneGuitar(std::string whichString);
+    //     3) Strum notes
+    void strumNotes (std::string whichChord);
+
+};
+
+struct Engines 
+{
+//     5 properties:
+//         1) Number of engines (int)
+    int numEngines = 2;
+//         2) Power output (float)
+    float powerOutput = 30.f;
+//         3) Fuel efficiency (float)
+    float fuelEfficiency = 10.f;
+//         4) Emissions (float)
+    float engineEmissions = 20.f;
+//         5) Size (int)
+    int engineSize = 20;
+    
+    struct Turbines
+    {
+        int lowPressureTurbineLenth = 84; 
+        int highPressureTurbineWidth = 93;
+        std::string turbineBrand = "GE";
+        std::string turbineModel = "CF3942";
+        std::string turbineMaterial ="Titanium";
+        
+        void turbineThrust(std::string whichTurbine);
+        void turbineEfficiency(std::string targetSpeed);
+        void turbineNoise(std::string whichTurbine);
+    };
+//     3 things it can do:
+//         1) Provide thrust
+    void engageThrust();
+//         2) Generate elecricity
+    void increasePower();
+//         3) Control speed and altitude
+    void increaseAltitude(int amountOfIncrease);
+};
+
 /*
 =================
 Part 1e - Step 10: Commit
